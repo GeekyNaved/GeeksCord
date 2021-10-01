facec70bfa80ef18be227cb6e043cd2a7d80a250
Naved <naved@40bears.com>
2021-10-01 10:14:07 +0530

    updating geekscord to geekyfi

--- App.js
+++ App.js
@@ -1,21 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from './component/HomeScreen.js';
+// import DetailScreen from './component/DetailScreen.js';
+// import CustomNavigationBar from './component/CustomNavigationBar.js';
+import LinksScreen from './component/LinksScreen.js';
 
-export default function App() {
+const Stack = createNativeStackNavigator();
+
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="Home"
+        >
+        <Stack.Screen name="home" component={HomeScreen} />
+        <Stack.Screen name="LinksScreen" component={LinksScreen} />
+      </Stack.Navigator> 
+    </NavigationContainer>
   );
 }
+export default App
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
